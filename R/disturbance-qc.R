3eae5521eddb56be137eb1185c7bad5aba0b1f16
Bailard <jbailard@nps.gov>
2021-09-21 17:50:51 -0700

    Created disturbance-qc file

---
+++ R/disturbance-qc.R
@@ -0,0 +1,31 @@
+# Function NYI: Overall disturbance < any other disturbance category
+
+# Function NYI: Flow modification exists, but no Human Use disturbance
+
+# Function NYI: Wildlife observed, no wildlife types
+
+# Function NYI: Wildlife type specified, no observations
+
+# Function NYI: Table with percent of springs with flow modification
+
+# Function NYI: Bar plot with percent of springs with flow modification
+
+# Function NYI: Table with percent of springs with anthro and natural disturbance (specifically human use, livestock)
+
+# Function NYI: Bar plot with percent of springs with anthro and natural disturbance (specifically human use, livestock)
+
+# Function NYI: Table with disturbance rankings 
+
+# Function NYI: Box plot with disturbance rankings
+
+# Function NYI: Table with livestock observations
+
+# Function NYI: Map of livestock observations
+
+# Function NYI: Table with human use observations
+
+# Function NYI: Map of human use observations
+
+# Function NYI: Table with ungulate observations
+
+# Function NYI: Map of ungulate observations
