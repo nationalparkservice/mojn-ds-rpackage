7b003bd8070da4096d07241baad7d2afc5e3fab0
Bailard <jbailard@nps.gov>
2021-09-21 17:51:04 -0700

    Created vegetation-qc file

---
+++ R/vegetation-qc.R
@@ -0,0 +1,13 @@
+# Function NYI: Vegetation observed, no lifeform
+
+# Function NYI: Lifeform present, no rank
+
+# Function NYI: If two lifeforms have same rank, next rank is blank
+
+# Function NYI: Table with summary of lifeform presence and rank
+
+# Function NYI: Bar plot and/or box plot with summary of lifeform presence and rank
+
+# Function NYI: Table of tamarisk, fountain grass, date palm, and fan palm (non-JOTR) observations
+
+# Function NYI: Map of tamarisk, fountain grass, date palm, and fan palm (non-JOTR) observations
