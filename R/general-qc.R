291cf1402a03624881804b732139862e8e652e17
Bailard <jbailard@nps.gov>
2022-05-16 12:32:18 -0700

    Added function for general map of annual and 3-year springs

--- R/general-qc.R
+++ R/general-qc.R
@@ -509,3 +509,84 @@ qcNotSampled <- function(conn, path.to.data, park, site, field.season, data.sour
   
   return(notsampled)
 }
+
+#' Map of annual and 3-year desert springs monitoring locations
+#'
+#' @param conn Database connection generated from call to \code{OpenDatabaseConnection()}. Ignored if \code{data.source} is \code{"local"}.
+#' @param path.to.data The directory containing the csv data exports generated from \code{SaveDataToCsv()}. Ignored if \code{data.source} is \code{"database"}.
+#' @param park Optional. Four-letter park code to filter on, e.g. "MOJA".
+#' @param site Optional. Site code to filter on, e.g. "LAKE_P_HOR0042".
+#' @param field.season Optional. Field season name to filter on, e.g. "2019".
+#' @param data.source Character string indicating whether to access data in the live desert springs database (\code{"database"}, default) or to use data saved locally (\code{"local"}). In order to access the most up-to-date data, it is recommended that you select \code{"database"} unless you are working offline or your code will be shared with someone who doesn't have access to the database.
+#'
+#' @return A leaflet map
+#' @export
+#'
+#' @examples
+LocationMap <- function(conn, path.to.data, park, site, field.season, data.source = "database") {
+  site <- ReadAndFilterData(conn = conn, path.to.data = path.to.data, park = park, site = site, field.season = field.season, data.source = data.source, data.name = "Site")
+  
+  coords <- site %>%
+    select(Park, SiteCode, SiteName, GRTSOrder, SiteStatus, SampleFrame, Lat_WGS84, Lon_WGS84, X_UTM_NAD83_11N, Y_UTM_NAD83_11N)
+
+  coords$SampleFrame <- factor(coords$SampleFrame, levels = c("Annual", "3Yr", "Over", "Inactive", "Rejected"))
+  
+  pal <- leaflet::colorFactor(palette = c("royalblue1", "red", "gold", "gray", "black"),
+                              domain = coords$SampleFrame)
+  
+  coords %<>%
+    dplyr::filter(SampleFrame %in% c("Annual", "3Yr"))
+  
+  # Make NPS map Attribution
+  NPSAttrib <-
+    htmltools::HTML(
+      "<a href='https://www.nps.gov/npmap/disclaimer/'>Disclaimer</a> |
+      &copy; <a href='http://mapbox.com/about/maps' target='_blank'>Mapbox</a>
+      &copy; <a href='http://openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors |
+      <a class='improve-park-tiles'
+      href='http://insidemaps.nps.gov/places/editor/#background=mapbox-satellite&map=4/-95.97656/39.02772&overlays=park-tiles-overlay'
+      target='_blank'>Improve Park Tiles</a>"
+    )
+  
+  NPSbasic = "https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck58pyquo009v01p99xebegr9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
+  NPSimagery = "https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck72fwp2642dv07o7tbqinvz4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
+  NPSslate = "https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck5cpvc2e0avf01p9zaw4co8o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
+  NPSlight = "https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck5cpia2u0auf01p9vbugvcpv/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
+  
+  width <- 800
+  height <- 800
+  
+  sitemap <- leaflet::leaflet(coords, height = height, width = width) %>%
+    leaflet::addTiles(group = "Basic", urlTemplate = NPSbasic, attribution = NPSAttrib) %>%
+    leaflet::addTiles(group = "Imagery", urlTemplate = NPSimagery, attribution = NPSAttrib) %>%
+    leaflet::addTiles(group = "Slate", urlTemplate = NPSslate, attribution = NPSAttrib) %>%
+    leaflet::addTiles(group = "Light", urlTemplate = NPSlight, attribution = NPSAttrib) %>%
+    leaflet::addScaleBar('bottomright') %>%
+    leaflet::addCircleMarkers(lng = ~Lon_WGS84,
+                              lat = ~Lat_WGS84,
+                              popup = paste ("Name: ", coords$SiteName, "<br>",
+                                             "Sample Frame: ", coords$SampleFrame, "<br>",
+                                             "Latitude (UTM): ", coords$X_UTM_NAD83_11N, "<br>",
+                                             "Longitude (UTM): ", coords$Y_UTM_NAD83_11N, "<br>",
+                                             "Latitude (Decimal Degrees): ", coords$Lat_WGS84, "<br>",
+                                             "Longitude (Decimal Degrees): ", coords$Lon_WGS84),
+                              radius = 6,
+                              stroke = TRUE,
+                              color = "black",
+                              weight = 2,
+                              fillOpacity = 0.8,
+                              fillColor = ~pal(SampleFrame),
+                              group = ~SampleFrame) %>%
+    leaflet::addLegend(pal = pal,
+                       values = ~SampleFrame,
+                       title = "Sample Frame",
+                       opacity = 1,
+                       position = "bottomleft") %>%
+    leaflet::addLayersControl(baseGroups = c("Basic", "Imagery", "Slate", "Light"),
+                              overlayGroups = ~SampleFrame,
+                              options=leaflet::layersControlOptions(collapsed = FALSE))
+  
+  return(sitemap)
+  
+}
+
