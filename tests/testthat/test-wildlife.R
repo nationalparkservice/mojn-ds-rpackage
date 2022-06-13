3f7b38232f79ba39943ec73a284387b2e785b803
Bailard <jbailard@nps.gov>
2022-06-13 12:02:42 -0700

    New general-qc test functions and other updates

--- tests/testthat/test-wildlife.R
+++ tests/testthat/test-wildlife.R
@@ -1,13 +1,12 @@
+context("Wildlife")
+
 test_that("qcWildlifeObservedNoTypes returns expected number of rows and columns", {
 
   actual_rows <- nrow(qcWildlifeObservedNoTypes(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
-  expected_rows <- 2
-  
-  expect_equal(actual_rows, expected_rows)
+  expect_equal(actual_rows, 2)
   
   actual_cols <- colnames(qcWildlifeObservedNoTypes(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
   expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "IsWildlifeObserved", "WildlifeType", "DirectObservation", "Scat", "Tracks", "Shelter", "Foraging", "Vocalization", "OtherEvidence", "Notes")
-  
   expect_equal(actual_cols, expected_cols)
     
 })
@@ -16,15 +15,11 @@ test_that("qcWildlifeObservedNoTypes returns expected number of rows and columns
 test_that("qcWildlifeObservedNoEvidence returns expected number of rows and columns", {
 
   actual_rows <- nrow(qcWildlifeObservedNoEvidence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
-  expected_rows <- 3
-  
-  expect_equal(actual_rows, expected_rows)
+  expect_equal(actual_rows, 3)
   
   actual_cols <- colnames(qcWildlifeObservedNoEvidence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
   expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "IsWildlifeObserved", "WildlifeType", "DirectObservation", "Scat", "Tracks", "Shelter", "Foraging", "Vocalization", "OtherEvidence", "Notes")
-  
   expect_equal(actual_cols, expected_cols)
-  
     
 })
 
@@ -32,18 +27,19 @@ test_that("qcWildlifeObservedNoEvidence returns expected number of rows and colu
 test_that("UngulatesEvidence returns expected number of rows and columns", {
 
   actual_rows <- nrow(UngulatesEvidence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(FieldSeason == "2020"))
-  expected_rows <- 37
-
-  expect_equal(actual_rows, expected_rows)
+  expect_equal(actual_rows, 37)
 
   actual_cols <- colnames(UngulatesEvidence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
   expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "IsWildlifeObserved", "WildlifeType", "DirectObservation", "Scat", "Tracks", "Shelter", "Foraging", "Vocalization", "OtherEvidence", "Notes")
-
   expect_equal(actual_cols, expected_cols)
   
   actual_type <- as.character(unique(UngulatesEvidence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::select(WildlifeType)))
-  expected_type <- "Ungulate"
+  expect_equal(actual_type, "Ungulate")
   
-  expect_equal(actual_type, expected_type)
+  actual_factor <- UngulatesEvidence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_false(any(sapply(actual_factor, class) == "factor"))
+  
+  actual_date <- UngulatesEvidence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_date$VisitDate), "Date")
     
 })
