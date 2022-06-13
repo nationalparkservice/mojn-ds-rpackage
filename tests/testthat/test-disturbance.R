bc0d3d3aed401f7de9aaf89e4af2d0b48a4ed99d
Bailard <jbailard@nps.gov>
2022-06-13 15:39:58 -0700

    Added vegetation, disturbance, and sensor test functions

---
+++ tests/testthat/test-disturbance.R
@@ -0,0 +1,151 @@
+context("Disturbance")
+
+test_that("qcOverallDisturbance returns correct number of rows and columns", {
+  
+  actual_rows <- nrow(qcOverallDisturbance(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 1)
+  
+  actual_cols <- colnames(qcOverallDisturbance(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "Roads", "HumanUse", "PlantManagement", "HikingTrails", "Livestock", "OtherAnthropogenic", "Fire", "Flooding", "Wildlife", "OtherNatural", "Overall")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_date <- qcOverallDisturbance(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_date$VisitDate), "Date")
+  
+})
+
+
+test_that("qcFlowModNoHuman returns correct number of rows and columns", {
+  
+  actual_rows <- nrow(qcFlowModNoHuman(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 32)
+  
+  actual_cols <- colnames(qcFlowModNoHuman(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "HumanUse", "FlowModificationStatus")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_date <- qcFlowModNoHuman(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_date$VisitDate), "Date")
+  
+})
+
+
+test_that("FlowModStatus works as expected", {
+  
+  actual_rows <- nrow(FlowModStatus(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 232)
+  
+  actual_cols <- colnames(FlowModStatus(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "FlowModificationStatus", "FlowModificationTypes")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_date <- FlowModStatus(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_date$VisitDate), "Date")
+  
+  actual_flowmods <- FlowModStatus(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(SiteCode == "MOJA_P_DOV0090", FieldSeason == "2019") %>% dplyr::select(FlowModificationStatus, FlowModificationTypes)
+  expected_flowmods <- as_tibble_row(c(FlowModificationStatus = as.character("Yes - One or more active"), FlowModificationTypes = as.character("Pipe, Qanat")))
+  expect_equal(actual_flowmods, expected_flowmods)
+  
+})
+
+
+test_that("qcFlowModDiscrepancies works as expected", {
+  
+  actual_rows <- nrow(qcFlowModDiscrepancies(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 77)
+  
+  actual_cols <- colnames(qcFlowModDiscrepancies(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "FlowModificationStatus", "FieldSeasons")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_fs <- qcFlowModDiscrepancies(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(SiteCode == "MOJA_P_TAL0187", FlowModificationStatus == "None") %>% dplyr::select(FlowModificationStatus, FieldSeasons)
+  expected_fs <- as_tibble_row(c(FlowModificationStatus = as.character("None"), FieldSeasons = as.character("2016, 2019, 2020, 2021")))
+  expect_equal(actual_fs, expected_fs)
+  
+})
+
+
+test_that("FlowModCount works as expected", {
+  
+  actual_rows <- nrow(FlowModCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 18)
+  
+  actual_cols <- colnames(FlowModCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "FlowModificationStatus", "Count", "Percent")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_int <- FlowModCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_int$Count), "integer")
+  
+  actual_dbl <- FlowModCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(typeof(actual_dbl$Percent), "double")
+  
+  actual_count <- FlowModCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(Park == "MOJA", FlowModificationStatus == "Yes - All inactive") %>% dplyr::select(Count)
+  expected_count <- as_tibble(as.integer(9)) %>% dplyr::rename(Count = value)
+  expect_equal(actual_count, expected_count)
+  
+  actual_percent <- FlowModCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(Park == "MOJA", FlowModificationStatus == "Yes - All inactive") %>% dplyr::select(Percent)
+  expected_percent <- as_tibble(as.double(9*100/45)) %>% dplyr::rename(Percent = value)
+  expect_equal(actual_count, expected_count)
+  
+})
+
+
+test_that("DisturbanceCount works as expected", {
+  
+  actual_rows <- nrow(DisturbanceCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 6)
+  
+  actual_cols <- colnames(DisturbanceCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "LivestockCount", "HumanUseCount", "LivestockPercent", "HumanUsePercent")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_int <- DisturbanceCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_int$LivestockCount), "integer")
+  expect_equal(class(actual_int$HumanUseCount), "integer")
+  
+  
+  actual_dbl <- DisturbanceCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(typeof(actual_dbl$LivestockPercent), "double")
+  expect_equal(typeof(actual_dbl$HumanUsePercent), "double")
+  
+  
+  actual_count <- DisturbanceCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(Park == "MOJA") %>% dplyr::select(LivestockCount, HumanUseCount)
+  expected_count <- as_tibble_row(c(LivestockCount = as.integer(21), HumanUseCount = as.integer(32)))
+  expect_equal(actual_count, expected_count)
+  
+  actual_percent <- DisturbanceCount(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(Park == "MOJA") %>% dplyr::select(LivestockPercent, HumanUsePercent)
+  expected_percent <- as_tibble_row(c(LivestockPercent = round(as.double(21*100/45), 1), HumanUsePercent = round(as.double(32*100/45), 1)))
+  expect_equal(actual_percent, expected_percent)
+  
+})
+
+
+test_that("HumanUseObservations returns correct number of rows and columns", {
+  
+  actual_rows <- nrow(HumanUseObservations(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 297)
+  
+  actual_cols <- colnames(HumanUseObservations(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "HumanUse", "Notes")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_date <- HumanUseObservations(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_date$VisitDate), "Date")
+  
+})
+
+
+test_that("LivestockObservations returns correct number of rows and columns", {
+  
+  actual_rows <- nrow(LivestockObservations(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 124)
+  
+  actual_cols <- colnames(LivestockObservations(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "Livestock", "Notes")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_date <- LivestockObservations(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_date$VisitDate), "Date")
+  
+})
---
+++ tests/testthat/test-vegetation.R
@@ -0,0 +1,98 @@
+context("Vegetation")
+
+test_that("qcVegPresentNoLifeforms returns correct number of rows and columns", {
+  
+  actual_rows <- nrow(qcVegPresentNoLifeforms(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 0)
+  
+  actual_cols <- colnames(qcVegPresentNoLifeforms(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "IsVegetationObserved", "LifeForm")
+  expect_equal(actual_cols, expected_cols)
+  
+})
+
+
+test_that("qcNoVegLifeformsPresent returns correct number of rows and columns", {
+ 
+  actual_rows <- nrow(qcNoVegLifeformsPresent(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 0)
+  
+  actual_cols <- colnames(qcNoVegLifeformsPresent(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "IsVegetationObserved", "LifeForm")
+  expect_equal(actual_cols, expected_cols)
+   
+})
+
+
+test_that("qcLifeformPresentNoRank works as expected", {
+
+  actual_rows <- nrow(qcLifeformPresentNoRank(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 0)
+  
+  actual_cols <- colnames(qcLifeformPresentNoRank(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "LifeForm", "Rank")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_int <- qcLifeformPresentNoRank(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_int$Rank), "integer")
+
+})
+
+
+test_that("qcLifeformRankCheck works as expected", {
+
+  actual_rows <- nrow(qcLifeformRankCheck(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 29)
+  
+  actual_cols <- colnames(qcLifeformRankCheck(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "Rank", "Count", "Diff", "LifeForms")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_int <- qcLifeformRankCheck(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_int$Rank), "integer")
+  expect_equal(class(actual_int$Count), "integer")
+  expect_equal(class(actual_int$Diff), "integer")
+  
+  actual_lifeforms <- qcLifeformRankCheck(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(SiteCode == "DEVA_P_LIT0005", FieldSeason == "2021", Rank == 6) %>% dplyr::select(LifeForms)
+  expected_lifeforms <- as_tibble(as.character("Sedge, Bryophyte")) %>% dplyr::rename(LifeForms = value)
+  expect_equal(actual_lifeforms, expected_lifeforms)
+   
+})
+
+
+test_that("LifeformsPresence works as expected", {
+
+  actual_rows <- nrow(LifeformsPresence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 267)
+  
+  actual_cols <- colnames(LifeformsPresence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "FieldSeason", "LifeForm", "Observations")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_int <- LifeformsPresence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_int$Observations), "integer")
+  
+  actual_observations <- LifeformsPresence(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(Park == "PARA", FieldSeason == "2020", LifeForm == "Bryophyte") %>% dplyr::select(Observations)
+  expected_observations <- as_tibble(as.integer(6)) %>% dplyr::rename(Observations = value)
+  expect_equal(actual_observations, expected_observations)
+    
+})
+
+
+test_that("InvasivePlants works as expected", {
+  
+  actual_rows <- nrow(InvasivePlants(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expect_equal(actual_rows, 125)
+  
+  actual_cols <- colnames(InvasivePlants(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local"))
+  expected_cols <- c("Park", "SiteCode", "SiteName", "VisitDate", "FieldSeason", "USDAPlantsCode", "ScientificName", "InRiparianVegBuffer", "Notes")
+  expect_equal(actual_cols, expected_cols)
+  
+  actual_date <- InvasivePlants(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local")
+  expect_equal(class(actual_date$VisitDate), "Date")
+  
+  actual_code <- InvasivePlants(path.to.data = here::here("tests", "testthat", "test_data"), data.source = "local") %>% dplyr::filter(SiteCode == "JOTR_P_COT0294", FieldSeason == "2020") %>% dplyr::select(USDAPlantsCode)
+  expected_code <- as_tibble(as.character(c("PHDA4", "TARA"))) %>% dplyr::rename(USDAPlantsCode = value)
+  expect_equal(actual_code, expected_code)
+  
+})
